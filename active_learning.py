c4acb0f988183bd4d0b0cd88b2b766f6a3bd330f
tanyinghui <yhtan927@gmail.com>
2020-12-14 12:41:22 +0800

    fix typo active_learning.py

--- active_learning.py
+++ active_learning.py
@@ -43,7 +43,7 @@ def active_learning_procedure(
         learner.teach(X_pool[query_idx], y_pool[query_idx])
         X_pool = np.delete(X_pool, query_idx, axis=0)
         y_pool = np.delete(y_pool, query_idx, axis=0)
-        model_accuracy_val = learner.score(X_val, y_val, verbose=0)
+        model_accuracy_val = learner.score(X_val, y_val)
         if (index + 1) % 5 == 0:
             print(f"Val Accuracy after query {index+1}: {model_accuracy_val:0.4f}")
         perf_hist.append(model_accuracy_val)
